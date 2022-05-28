d97786bac3d55ca0b34c9386f60b5e0083745081
gagneja1512 <aadityaarora1215@gmail.com>
2022-05-29 00:16:25 +0530

    Wrapping up by implementing the highscore

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random()*20) + 1 ;
 let score = 20; 
+let highscore = 0;
 document.querySelector('.number').textContent = '?' ;
 
 document.querySelector('.check').addEventListener('click' , function(){
@@ -19,6 +20,12 @@ document.querySelector('.check').addEventListener('click' , function(){
         document.querySelector('body').style.backgroundColor = '#60b347' ;
         document.querySelector('.number').style.width = '30rem' ;
         document.querySelector('.number').textContent = guess ;
+
+        if(score > highscore)
+        {
+            highscore = score ;
+            document.querySelector('.highscore').textContent = highscore ;
+        }
     }
 
     else if(guess > secretNumber)
