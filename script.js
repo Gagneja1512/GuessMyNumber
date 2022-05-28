88ec9079b5c6b9b35eb8b201b8bfb80dba5c03a8
gagneja1512 <aadityaarora1215@gmail.com>
2022-05-28 23:59:12 +0530

    Adding again functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random()*20) + 1 ;
+let secretNumber = Math.trunc(Math.random()*20) + 1 ;
 let score = 20; 
 document.querySelector('.number').textContent = '?' ;
 
@@ -50,4 +50,17 @@ document.querySelector('.check').addEventListener('click' , function(){
             document.querySelector('.score').textContent = 0 ;
         }
     }
-})
+}) ;
+
+document.querySelector('.again').addEventListener('click' , function(){
+    score = 20 ;
+    secretNumber = Math.trunc(Math.random()*20) + 1 ;
+
+    document.querySelector('.message').textContent = 'Start guessing...' ;
+    document.querySelector('.score').textContent = score ;
+    document.querySelector('.number').textContent = '?' ;
+    document.querySelector('.guess').value = '' ;
+
+    document.querySelector('body').style.backgroundColor = '#222' ;
+    document.querySelector('.number').style.width = '15rem' ;
+}) ;
