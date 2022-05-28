650e6a70a017de3335a373a5aa47b9eb42191951
gagneja1512 <aadityaarora1215@gmail.com>
2022-05-28 23:28:13 +0530

    Added jS logic and Handling click events

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random()*20) + 1 ;
+let score = 20; 
+document.querySelector('.number').textContent = secretNumber ;
+
+document.querySelector('.check').addEventListener('click' , function(){
+    const guess = Number(document.querySelector('.guess').value) ;
+
+    if(!guess)
+    {
+        document.querySelector('.message').textContent = 'No Number !' ;
+    }  
+    
+    else if(guess === secretNumber)
+    {
+        document.querySelector('.message').textContent = 'Correct Number :)' ;
+    }
+
+    else if(guess > secretNumber)
+    {
+        if(score > 1)
+        {
+            document.querySelector('.message').textContent = 'Too High !' ;
+            score = score - 1 ;
+            document.querySelector('.score').textContent = score ;
+        }
+        else 
+        {
+            document.querySelector('.message').textContent = 'You lost the Game :(' ;
+            document.querySelector('.score').textContent = 0 ;
+        }
+    }
+
+    else if(guess < secretNumber)
+    {   
+        if(score > 1)
+        {
+            document.querySelector('.message').textContent = 'Too low !' ;
+            score = score - 1 ;
+            document.querySelector('.score').textContent = score ;
+        }
+        else 
+        {
+            document.querySelector('.message').textContent = 'You lost the Game :(' ;
+            document.querySelector('.score').textContent = 0 ;
+        }
+    }
+})
