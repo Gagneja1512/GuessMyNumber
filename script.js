05284c46bc90e39eda0795cfd0c8d2103f009424
gagneja1512 <aadityaarora1215@gmail.com>
2022-05-28 23:46:20 +0530

    Handling CSS styles

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const secretNumber = Math.trunc(Math.random()*20) + 1 ;
 let score = 20; 
-document.querySelector('.number').textContent = secretNumber ;
+document.querySelector('.number').textContent = '?' ;
 
 document.querySelector('.check').addEventListener('click' , function(){
     const guess = Number(document.querySelector('.guess').value) ;
@@ -15,6 +15,10 @@ document.querySelector('.check').addEventListener('click' , function(){
     else if(guess === secretNumber)
     {
         document.querySelector('.message').textContent = 'Correct Number :)' ;
+
+        document.querySelector('body').style.backgroundColor = '#60b347' ;
+        document.querySelector('.number').style.width = '30rem' ;
+        document.querySelector('.number').textContent = guess ;
     }
 
     else if(guess > secretNumber)
